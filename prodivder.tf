f6a523b8499f329aecaa14d118028d3d855dc849
tiagodfigueiredo7 <tiagod.figueiredo@gmail.com>
2022-06-24 01:23:49 +0000

    teste

--- prodivder.tf
+++ prodivder.tf
@@ -1,4 +1,12 @@
-# Configure the AWS Provider
-provider "aws" {
-  region = "us-east-2"
+#aqui é onde escolhemos qual provedor o Terraform irá trabalhar - AWS
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
 }
+# aqui é onde escolhemos a região e as Key do usuario para criação IAM
+provider "aws" {
+  region     = "us-east-2"
